d3828f096102b750f52f9da33a971da4a8032dc4
Carolina Urrea <curestrepo@gmail.com>
2023-01-26 15:33:45 -0800

    pr commit

--- cypress/e2e/1-getting-started/flakeyTest.cy.js
+++ cypress/e2e/1-getting-started/flakeyTest.cy.js
@@ -3,8 +3,3 @@ it('should fail then pass', { retries: 3 }, () => {
     count++
     expect(count).eq(2)
 })
-it('should fail then pass', { retries: 3 }, () => {
-    let count = 0
-    count++
-    expect(count).eq(3)
-})
